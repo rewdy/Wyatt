d7acb83f0bf0b8fa3d1985dc643f863634562ff9
Andrew Meyer <ameyer@egov.com>
2015-10-26 12:45:53 -0500

    Only show featured on singular post.

--- partials/content.php
+++ partials/content.php
@@ -15,14 +15,18 @@ Article default template file
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 	<?php
-	$featured_image = wyatt_featured_image();
-	if ($featured_image) :
+	if (is_singular()) :
+		$featured_image = wyatt_featured_image();
+		if ($featured_image) :
 	?>
+
+	<!-- Featured image -->
 	<div class="featured-image">
 		<img src="<?php echo $featured_image['url']; ?>" width="<?php echo $featured_image['width']; ?>" height="<?php echo $featured_image['height']; ?>" />
 	</div>
-	<?php endif; ?>
-
+	<?php endif; // end if featured image set ?>
+	<?php endif; // end if singular ?>
+	
 	<!-- Post Title -->
 	<?php if (!is_singular()) :?>
 	<h2><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
